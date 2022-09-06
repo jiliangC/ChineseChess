429a969ccece0eaa92e3db31c8c9a819279fa23c
jiliangC <1327085656@qq.com>
2022-09-06 23:34:57 +0800

    测试

--- src/gxu/data_structure/chess/ChessGame.java
+++ src/gxu/data_structure/chess/ChessGame.java
@@ -5,6 +5,7 @@ package gxu.data_structure.chess;
  */
 public class ChessGame {
     public static void main(String[] args) {
+        System.out.println(11);
         new PrimaryPageWindow().setVisible(true);
     }
 }
